9e3dfc72d3d7dcc01a478b78d0ee04d3a1cdc8b6
TheRomeroGuilherme <guilherme.g4m3s@gmail.com>
2022-11-24 02:55:20 -0300

    arrumando

---
+++ index.js
@@ -0,0 +1,4 @@
+/*colocar alerta para a pessoa falar que a senha está errada, para que ela mude,
+vai ter que aperta botão mudar senha, escrever o email dela para poder trocar de senha
+ou mandar a senha para ela.
+*/
